1a6a47ed1c43bb774830ffed3a3164451859f0b2
girishkuwar <girishkuwar11.com>
2025-07-26 22:15:04 +0530

    final

--- routes/excelRoutes.js
+++ routes/excelRoutes.js
@@ -7,7 +7,7 @@ const { default: mongoose } = require("mongoose");
 router.post("/save", authenticateUser, async (req, res) => {
   try {
     const userID = req.user.id;
-    const { data , name} = req.body;
+    const { data, name } = req.body;
 
     if (!data) {
       return res.status(400).json({ message: "Excel data is required" });
@@ -31,11 +31,35 @@ router.post("/save", authenticateUser, async (req, res) => {
 router.get("/getsheets", authenticateUser, async (req, res) => {
   try {
     const userID = req.user.id;
-    const uploads = await ExcelData.find({ user: userID },{ data: 0 }).sort({ uploadedAt: -1 });
+    const uploads = await ExcelData.find({ user: userID }, { data: 0 }).sort({
+      uploadedAt: -1,
+    });
     res.status(200).json(uploads);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.get("/:id", authenticateUser, async (req, res) => {
+  try {
+    const upload = await ExcelData.findById(req.params.id);
+
+    if (!upload) {
+      return res.status(404).json({ message: "Upload not found" });
+    }
+    if (upload.user.toString() !== req.user.id) {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    res.json({
+      data: upload.data,
+      name: upload.fileName,
+      createdAt: upload.createdAt
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
   }
 });
 
