187ff64181513c352ffbad7d25fca8547c3827e7
girishkuwar <girishkuwar11.com>
2025-07-26 20:09:18 +0530

    exel user data api created

--- routes/excelRoutes.js
+++ routes/excelRoutes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const ExcelData = require("../models/ExcelData");
 const { authenticateUser } = require("../middleware/auth");
+const { default: mongoose } = require("mongoose");
 
 router.post("/save", authenticateUser, async (req, res) => {
   try {
     const userID = req.user.id;
-    const { data } = req.body;
+    const { data , name} = req.body;
 
     if (!data) {
       return res.status(400).json({ message: "Excel data is required" });
@@ -14,6 +15,7 @@ router.post("/save", authenticateUser, async (req, res) => {
 
     const newEntry = new ExcelData({
       user: userID,
+      fileName: name,
       data,
       uploadedAt: new Date(),
     });
@@ -22,6 +24,17 @@ router.post("/save", authenticateUser, async (req, res) => {
     res.status(201).json({ message: "Data saved successfully" });
   } catch (err) {
     console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.get("/getsheets", authenticateUser, async (req, res) => {
+  try {
+    const userID = req.user.id;
+    const uploads = await ExcelData.find({ user: userID },{ data: 0 }).sort({ uploadedAt: -1 });
+    res.status(200).json(uploads);
+  } catch (error) {
+    console.error(error);
     res.status(500).json({ message: "Server error" });
   }
 });
